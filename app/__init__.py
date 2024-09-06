6f38d1bfda870be1df83b67c26e2e2a6ce381c9c
AyishND <ayishnad@gmail.com>
2024-09-06 10:45:39 +0500

    cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:admin@localhost:5432/ehr"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = "b52264934f8dc66bba2a9f9b28871d0993a728f0be4ab165ab4b034af3219184"
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=7)  # Access token expires in 15 minutes
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=7) 
 
     db.init_app(app)
     migrate.init_app(app, db)
